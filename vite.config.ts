09219adb397a33f4b1062f096795b1a379cc4c97
Dmytro Velychko <surokdimkus@gmail.com>
2024-03-02 15:16:51 +0200

    Added a new custom tab to Chrome Devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { crx } from "@crxjs/vite-plugin";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+
+import manifest from "./manifest.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), crx({ manifest })],
+  build: {
+    rollupOptions: {
+      input: {
+        // Make vite bundle these pages
+        devtools: "devtools_page.html",
+        index: "index.html",
+      },
+    },
+  },
+});
